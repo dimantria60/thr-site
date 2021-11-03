17f7203ff76d36e4cd7ee2eb4d3ba4b42c725252
Luis Rosales <luisangel@gmail.com>
2021-11-03 17:44:06 -0600

    add statics

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layaut';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="Home Page">
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      </Layout>
+    </main>
+  )
+}
+export default AboutPage
