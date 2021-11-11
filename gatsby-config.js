9b95ce3dd9a3f1d905494941fe2fde62a8f2a5de
Luis Rosales <luisangel@gmail.com>
2021-11-11 16:07:51 -0600

    hola mundo

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,20 +8,16 @@ module.exports = {
 		'gatsby-plugin-sharp',
 		`gatsby-plugin-sass`,
 		{
-			resolve: `gatsby-plugin-sharp`,
+			resolve: `gatsby-plugin-manifest`,
 			options: {
-				icon: 'src/images/favicon.ico',
-				// Defaults used for gatsbyImageData and StaticImage
-				defaults: {},
-				// Set to false to allow builds to continue on image errors
-				failOnError: true,
-				// deprecated options and their defaults:
-				base64Width: 20,
-				//forceBase64Format: `png,jpg,ico`, // valid formats: png,jpg,webp
-				useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
-				stripMetadata: true,
-				defaultQuality: 50,
+				name: 'React go',
+				short_name: 'Reactgo',
+				start_url: '/',
+				background_color: '#f7f0eb',
+				theme_color: '#a2466c',
+				display: 'standalone',
+				icon: 'src/images/image.png',
 			},
-		},
+		}
 	],
 };
